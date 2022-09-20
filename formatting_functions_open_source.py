ac1d90446794b0572c5a38ffd8866a4458b0069e
Charlie Beckler <47010787+cbeckler@users.noreply.github.com>
2022-09-20 09:51:01 -0700

    added format_header() function

--- formatting_functions_open_source.py
+++ formatting_functions_open_source.py
@@ -1,5 +1,46 @@
 # SUITE OF FUNCTIONS TO AUTOMATE EXCEL REPORT FORMATTING WITH XLSXWRITER
 
+def format_header(df, wb, sheet,  bg_color1, font_color1, bg_color2, font_color2):
+
+    # This function will apply formatting to your header row    
+    ## Color for index can also be set to be different, or the same as normal header columns
+    ### This function should be applied to data that has already been loaded into a worksheet via to_excel()
+    ### Meant only for dataframes with single row index and columns 
+
+    # ARGUMENTS
+    
+    ## MANDATORY:
+    ### df is your data from your dataframe
+    ### wb is your workbook
+    ### sheet is your worksheet
+    ### all color args can be added with keywords (ie, 'red') but hex codes (ex '#FF0000') are better for customization
+    ### bg_color1 is the background color for your column headers
+    ### font_color1 is the font color for your column headers
+    ### bg_color2 is the background color for your index header
+    ### font_color2 is the font color for your index headers
+
+    # create format templates
+    header_format = wb.add_format({'bold':True,'bg_color':bg_color1,'font_color':font_color1,'align':'center','bottom':True})
+
+    ## the header_format template is applied in the first row for all columns, which also keeps the value from the df header row
+    ## the for loop goes over all columns. this prevents the formatting being applied to empty cells
+    ### using enumerate and calling values will extract the column value (in this case, column header)
+    for col_num, value in enumerate(df.columns.values):
+        # normal header formatting is applied to all header columns
+        ## col_num + 1 here is so that formatting is applied to the column headers only
+        sheet.write(0, col_num + 1, value, header_format)
+
+    # the header loop cannot be applied to the index, so formatting is manually applied by overwriting the cell 
+    ## also allowing me to add R border to it only
+    index_format = wb.add_format({'bold':True,'bg_color':bg_color2,'font_color':font_color2,'align':'left','bottom':True,'right':True}) 
+    # the name of the index is selected and to be put in cell A1
+    df_index_name = df.index.name 
+    # instead of writing by row_number, column_number, it is possible to write to the specific cell (A1)
+    ## this is only recommended if this cell will be the same every time
+    sheet.write('A1', df_index_name, index_format)
+
+
+
 def last_col_highlight_header(df, wb, sheet, bg_color1, font_color1, bg_color2, font_color2, bg_color3, font_color3):
 
     # This function will apply formatting to your headers that will automatically apply a different color to your last column to highlight it
