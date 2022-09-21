f387900eecacf9af2d4af2a6994459421332d26d
Charlie Beckler <47010787+cbeckler@users.noreply.github.com>
2022-09-20 19:21:43 -0500

    add color defaults

--- formatting_functions_open_source.py
+++ formatting_functions_open_source.py
@@ -4,10 +4,10 @@
 
 ###                 SINGLE ROW INDEX AND COLUMNS DATAFRAMES                 ###
 
-def format_header(df, wb, sheet,  bg_color1, font_color1, bg_color2, font_color2):
+def format_header(df, wb, sheet,  bg_color1='#002387', font_color1='#FFFFFF', bg_color2='#002387', font_color2='#FFFFFF'):
 
     # This function will apply formatting to your header row    
-    ## Color for index can also be set to be different, or the same as normal header columns
+    ## Color for index can also be set to be different, or the same as normal header columns. It is the same by default
     ### This function should be applied to data that has already been loaded into a worksheet via to_excel()
     ### Meant only for dataframes with single row index and columns 
 
@@ -17,11 +17,13 @@ def format_header(df, wb, sheet,  bg_color1, font_color1, bg_color2, font_color2
     ### df is your data from your dataframe
     ### wb is your workbook
     ### sheet is your worksheet
+    
+    ## OPTIONAL:
     ### all color args can be added with keywords (ie, 'red') but hex codes (ex '#FF0000') are better for customization
-    ### bg_color1 is the background color for your column headers
-    ### font_color1 is the font color for your column headers
-    ### bg_color2 is the background color for your index header
-    ### font_color2 is the font color for your index headers
+    #### bg_color1 is the background color for your column headers
+    #### font_color1 is the font color for your column headers
+    #### bg_color2 is the background color for your index header
+    #### font_color2 is the font color for your index headers
 
     # create format templates
     header_format = wb.add_format({'bold':True,'bg_color':bg_color1,'font_color':font_color1,'align':'center','bottom':True})
@@ -45,11 +47,11 @@ def format_header(df, wb, sheet,  bg_color1, font_color1, bg_color2, font_color2
 
 
 
-def last_col_highlight_header(df, wb, sheet, bg_color1, font_color1, bg_color2, font_color2, bg_color3, font_color3):
+def last_col_highlight_header(df, wb, sheet, bg_color1='#002387', font_color1='#FFFFFF', bg_color2='#00A111', font_color2='#FFFFFF', bg_color3='#002387', font_color3='#FFFFFF'):
 
     # This function will apply formatting to your headers that will automatically apply a different color to your last column to highlight it
     ## This is especially useful for time series: highlighting most recent year etc
-    ## Color for index can also be set to be different, or the same as normal header columns
+    ## Color for index can also be set to be different, or the same as normal header columns. Is the same as normal by default
     ### This function should be applied to data that has already been loaded into a worksheet via to_excel() from dataframe
     ### Meant only for dataframes with single row index and columns
 
@@ -59,13 +61,15 @@ def last_col_highlight_header(df, wb, sheet, bg_color1, font_color1, bg_color2, 
     ### df is your data from your dataframe
     ### wb is your workbook
     ### sheet is your worksheet
+    
+    ## OPTIONAL:
     ### all color args can be added with keywords (ie, 'red') but hex codes (ex '#FF0000') are better for customization
-    ### bg_color1 is the background color for your column headers
-    ### font_color1 is the font color for your column headers
-    ### bg_color2 is the background color for your LAST column header
-    ### font_color2 is the font color for your LAST column header
-    ### bg_color3 is the background color for your index header
-    ### font_color3 is the font color for your index headers
+    #### bg_color1 is the background color for your column headers
+    #### font_color1 is the font color for your column headers
+    #### bg_color2 is the background color for your LAST column header
+    #### font_color2 is the font color for your LAST column header
+    #### bg_color3 is the background color for your index header
+    #### font_color3 is the font color for your index headers
 
     # getting column count of the data to use to set upper bound for formatting
     df_column_count = len(df.columns)
@@ -80,7 +84,7 @@ def last_col_highlight_header(df, wb, sheet, bg_color1, font_color1, bg_color2, 
     ### using enumerate and calling values will extract the column value (in this case, column header)
     for col_num, value in enumerate(df.columns.values):
         # because col_num starts at 0 in python, 1 must be added to it so that number of the last column equals the column count
-        # the special latest_period formatting will only be applied to the last column
+        # the special last_col_format formatting will only be applied to the last column
         if col_num + 1 == df_column_count:
             # the first argument of 0 specifies this will be applied to the first row of the excel spreadsheet
             ## col_num + 1 here is so that formatting is applied to the column headers only
@@ -165,9 +169,6 @@ def format_single_numeric_data_type_df(df, wb, sheet, data_type, col_width=14):
     #       'percent_2' = decimal percentage to hundredths (ex 20.00%)
         
     ## OPTIONAL:
-    ## these arguments have default values but can be specified if you want to change them from defaults
-    ## when changing them, you MUST type the argument = 
-    ## ie, format_single_numeric_data_type_df(df, wb, sheet, data_type, col_width=24)
     ### col_width is the width of the data columns
 
     # this if statement sets the formatting based off the data_type argument
