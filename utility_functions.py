95bca65587abdb463ba6af348bedd31a3301f317
Charlie Beckler <47010787+cbeckler@users.noreply.github.com>
2022-09-26 16:35:19 -0500

    added clean_header_string()

--- utility_functions.py
+++ utility_functions.py
@@ -20,3 +20,27 @@ def return_divisible_ints(start_num, end_num, denominator):
             divisible_ints.append(i)
     
     return divisible_ints
+
+
+def clean_header_string(string):
+
+    # this function will convert an underscore separted or camelcase string to title format
+    ## ex: "birth_date" or "BirthDate" to "Birth Date"
+
+    # MANDATORY:
+    ## string is the string to convert
+
+    import re
+
+    # if string contains underscores
+    #if re.fullmatch(r'^([a-z][A-Z][0-9]+)(_[a-z][A-Z][0-9]+)+$', string) != None:
+    if '_' in string:
+        # replace underscore with space and capitalize all first letters
+        return string.title().replace('_',' ')
+    # # if string is camelcase
+    elif re.fullmatch(r'(?:[A-Z][a-z]+)+', string) != None:
+        # insert space before the next captalized letter
+        return re.sub(r'((?<=[a-z])[A-Z]|(?<!\A)[A-Z](?=[a-z]))', r' \1', string)
+    else: 
+        # else return string with first letter capitalized
+        return string.title()
